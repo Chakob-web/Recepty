063945a1227afd5318028f4daccc251b8d45c08f
Jakub Hejl <HejlJakub@litv.sssvt.cz>
2025-01-15 09:12:03 +0100

    first commit

---
+++ Hejl_receptar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hejl_receptar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
